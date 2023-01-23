3ec7b2670b940c5aad5d4684a3c0d0e9c9c66853
rabobahago <rabobahago@gmail.com>
2023-01-23 04:48:05 +0100

    fulfillRandowWords and requestNft method implemented

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -2,25 +2,51 @@
 pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract RandomIpfsNft is VRFConsumerBaseV2 {
+contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     uint64 private immutable i_subscriptionId;
     bytes32 private immutable i_gaslane;
     uint32 private immutable i_callbackGasLimit;
-    uint16 private constant i_REQUEST_CONFIRMATIONS = 3;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
+    //VRF helper
+    mapping(uint256 => address) public s_requestIdToSender;
+    //NFT token Counter Variable;
+    uint256 public s_tokenCounter;
 
-    constructor(address vrfCoordinatorV2) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS NFT", "RIN") {
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_subscriptionId = subscriptionId;
+        i_gaslane = gasLane;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
-    function requestNft() public {}
+    function requestNft() public returns (uint256 requestId) {
+        requestId = i_vrfCoordinator.requestRandomWords(
+            i_gaslane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        s_requestIdToSender[requestId] = msg.sender;
+    }
 
     function fulfillRandowWords(uint256 requestId, uint256[] memory randomWords)
         internal
         override
-    {}
+    {
+        address dogOwner = s_requestIdToSender[requestId];
+        uint256 newTokenId = s_tokenCounter;
+        _safeMint(dogOwner, newTokenId);
+    }
 
     function tokenUri(uint256) public {}
 }
