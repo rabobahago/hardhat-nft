c261eae0483d6384b6d5d30b9fd20bb07ac48ae7
rabobahago <rabobahago@gmail.com>
2023-01-24 07:03:33 +0100

    random IPFS NFT getBreedFromModdedRng implemented

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -3,8 +3,15 @@ pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+error RandomIpfsNft__RangeOutOfBounds();
 
 contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
+    //type declaration
+    enum Breed {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
+    }
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     uint64 private immutable i_subscriptionId;
     bytes32 private immutable i_gaslane;
@@ -13,8 +20,9 @@ contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
     uint32 private constant NUM_WORDS = 1;
     //VRF helper
     mapping(uint256 => address) public s_requestIdToSender;
-    //NFT token Counter Variable;
+    //NFT Variable;
     uint256 public s_tokenCounter;
+    uint256 internal constant MAX_CHANCE_VALUE = 100;
 
     constructor(
         address vrfCoordinatorV2,
@@ -45,8 +53,34 @@ contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
     {
         address dogOwner = s_requestIdToSender[requestId];
         uint256 newTokenId = s_tokenCounter;
+
+        uint256 moddedRng = randomWords[0] % MAX_CHANCE_VALUE;
+        Breed dogBreed = getBreedFromModdedRng(moddedRng);
         _safeMint(dogOwner, newTokenId);
     }
 
-    function tokenUri(uint256) public {}
+    function getBreedFromModdedRng(uint256 moddedRng)
+        public
+        pure
+        returns (Breed)
+    {
+        uint256 cummulativeSum = 0;
+        uint256[3] memory chanceArray = getChanceArray();
+        for (uint256 i = 0; i < chanceArray.length; i++) {
+            if (
+                moddedRng >= cummulativeSum &&
+                moddedRng < cummulativeSum + chanceArray[i]
+            ) {
+                return Breed(i);
+            }
+            cummulativeSum += chanceArray[i];
+        }
+        revert RandomIpfsNft__RangeOutOfBounds();
+    }
+
+    function getChanceArray() public pure returns (uint256[3] memory) {
+        return [10, 30, MAX_CHANCE_VALUE];
+    }
+
+    function tokenUri(uint256) public view override returns (string memory) {}
 }
